fdee1c65d37b5d14e1f7b58f5691afa0096556cc
Jared <jarodd420@gmail.com>
2026-03-01 16:23:44 -0500

    fix

--- path-app/public/app.js
+++ path-app/public/app.js
@@ -901,6 +901,7 @@ function init() {
     const overlay = $('#contact-card-overlay');
     const url = overlay?.dataset?.contactCardAvatarUrl;
     if (url) {
+      closeContactCard();
       openImageLightbox(url);
     }
   });
